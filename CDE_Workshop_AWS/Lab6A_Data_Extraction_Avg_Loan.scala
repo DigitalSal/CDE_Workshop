e36c21db66e70c659ee58cc7dcc2b4bb940ad4a9
DigitalSal <32275459+DigitalSal@users.noreply.github.com>
2022-10-24 10:00:08 -0500

    Add files via upload

---
+++ CDE_Workshop_AWS/Lab6A_Data_Extraction_Avg_Loan.scala
@@ -0,0 +1,30 @@
+//
+//Copyright (c) 2020 Cloudera, Inc. All rights reserved.
+//
+
+import org.apache.spark.sql.SparkSession
+
+//---------------------------------------------------
+//              CREATE SPARK SESSION
+//---------------------------------------------------
+val user = System.getProperty("user.name")
+val appName = user + "-AvgLoan-Texas"
+val spark = SparkSession.builder.appName(appName).getOrCreate()
+
+// Update s3 details here
+val input_path ="s3a://pjoshicdpoct2021/data"
+
+val base_df=spark.read.option("header","true").option("inferSchema","true").csv(input_path + "/PPP-Sub-150k-TX.csv")
+
+//------------------------------------------------------------------------------------------------------
+//               SELECT RELEVANT COLUMNS AND GET THE AVG LOAN AMOUNT FOR A CITY, LENDER
+//------------------------------------------------------------------------------------------------------
+
+val filtered_df = base_df.select("LoanAmount", "City", "State", "Zip", "BusinessType", "NonProfit", "JobsRetained", "DateApproved", "Lender")
+
+val final_df = filtered_df.groupBy("Lender", "City").agg(avg($"LoanAmount").as("Average_Loan_Amount"))
+
+print("Average loan amount by a lender in Texas:- ")
+print("........................")
+
+final_df.show()
---
+++ CDE_Workshop_AWS/README.md
@@ -0,0 +1,3 @@
+# CDE_Workshop_AWS
+______________________________________________________________________________________________________________________________________________________
+### Repository to keep CDE Workshop exercises. The scripts use s3 as the source of datasets as this is created for CDE on AWS. 
